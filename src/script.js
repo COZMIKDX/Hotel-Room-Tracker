0473d3c0cd97b82887791d7b6ffeaa89303b945d
COZMIKDX <tacocatdx@gmail.com>
2023-07-12 17:25:21 -0500

    Added room matrix visual. All at once because I did it on codepen.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Program Flow info will be updated and moved to use a more appropriate documentation method.
 // Flow 1: Get text from one of the main textareas. sanitizeText() gets rid of \n, whitespace, and pushes to
 // the element to an array. sortList() converts the elements to Numbers and sorts the array.
 // updateListBox() reinserts the \n in each element, builds a string with them, and updates the textarea with this string.
@@ -6,19 +7,45 @@
 // Flow 2: Restoring; Get the textarea under the same parent node as the child object. Grab the text in the textarea.
 // Reformat it such that the commas and spaces are replaced with newlines. Place in the left textarea box
 
-// Next Objective: Check the right textarea for repeats after sorting. Remove them.
-// When adding the new rooms to the left textarea, check for repeats again and remove them.
-// Clear the right textarea when you hit the log button.
+// TODO: Look through code for lines that are similar and reused.
+//  I could make generic versions as functions and replace those lines with the functions.
+//  I saw someone call those sort of functions hooks but I'm not sure that was right or not.
 
 let oldList = document.querySelector("#oldList");
 let newList = document.querySelector("#newList");
 let oldButton = document.querySelector("#oldButton");
 let newButton = document.querySelector("#newButton");
-let logList = document.querySelector("#log-list");
+let matrixEditButton = document.querySelector("#matrixEditButton");
+let matrixSetButton = document.querySelector("#matrixSetButton");
+let setCheckoutInput = document.querySelector("#setCheckoutInput");
+let testButton = document.querySelector("#testButton");
+let testButton2 = document.querySelector("#testButton2");
+let logList = document.querySelector(".log-list");
+let matrix = document.querySelector(".matrix");
 
-function sanitizeText(text) {
+let editMode = false;
+// I would have used hex codes but the color code when grabbing from a DOM element returns in the rgb(r,g,b) format.
+// I may write a function to convert between the two but for now this will do.
+const roomColors = {
+  "dueOut": "rgb(255, 97, 97)",
+  "checkedOut": "rgb(186, 187, 255)",
+  "earlyCheckout": "rgb(66, 245, 170",
+  "misc": "rgb(237, 237, 237)"
+};
+
+let startFloor = 2;
+let endFloor = 6;
+let floorRoomCount = {
+  2: 22,
+  3: 25,
+  4: 25,
+  5: 25,
+  6: 25
+};
+
+function sanitizeText(text, delimiter) {
   //console.log(text);
-  let splitText = text.split("\n");// returns an array of strings.  
+  let splitText = text.split(`${delimiter}`);// returns an array of strings.  
   // Trim the whitespace
   let trimText = [];
   for (let str of splitText) { // Use a for of loop, not a for in loop.
@@ -78,6 +105,7 @@ function sortList(list) {
 function repeatCheck(list) {
   let prevNum = 0; // I'll switch this to null or whatever javascript uses later.
   let noRepeatList = [];
+  let newCheckoutsList = [];
   // input array should already be sorted. Otherwise this won't work.
   for (let num of list) {
     if (prevNum != num) {
@@ -100,11 +128,13 @@ function listLoadFormat(text) {
   return formattedText;
 }
 
-// TODO: updateListBox already inserts newlines, so I end up with double newlines currently.
-// It currently takes and array and converts it to a string. I think I need to rework that function to take strings and
-// I'll do whatever array to string or backwards stuff before I call it.
+// TODO: come up with a way to get event target's (restore button) associated main list 
+// without having to grab the parentNode and its childNodes.
+// Doing it that way means I would need to adjust how I grab the textarea object each time
+// I modify the DOM elements I'm making, which is bad.
 function restore(event) {
-  let textbox = event.target.parentNode.childNodes[2];
+  let textbox = event.target.parentNode.childNodes[2].childNodes[0];
+  console.log(textbox);
   updateListBox(oldList, listLoadFormat(textbox.value));
 }
 
@@ -146,6 +176,7 @@ function addLog(sortedList, newCheckoutList) {
   let restoreButton = document.createElement("input");
   
   entry.setAttribute("class", "log-entry");
+  listArea.setAttribute("class", "list-area");
   
   restoreButton.setAttribute("type", "button");
   restoreButton.value = "Restore";
@@ -174,33 +205,146 @@ function addLog(sortedList, newCheckoutList) {
   restoreButton.addEventListener("click", restore);
 }
 
+function roomNumberFormat(floorNumber, roomNumber) {
+  return `${floorNumber}${roomNumber.toString().padStart(2,"0")}`
+}
+// Draws room matrix. Only the cells with room number.
+function drawMatrix() {
+  matrix.setAttribute("class", "matrix");
+  // I'll either make a div for each row and use flexbox or 
+  // I'll try using flexbox wrapping.
+  
+  for (let i = startFloor; i <= endFloor; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "matrix-row");
+    for (let j = 0; j <= floorRoomCount[i]; j++) {
+      drawCell(`${i}${j.toString().padStart(2,'0')}`, row);
+    }
+    matrix.append(row);
+  }
+  
+}
+
+function drawCell(roomNumber, parent) {
+  let cell = document.createElement("div");
+  cell.setAttribute("class", "matrix-cell");
+  cell.setAttribute("id", `room-${roomNumber}`);
+  cell.style.backgroundColor = roomColors.misc;
+  //cell.innerHTML = `${roomNumber}`;
+  cell.addEventListener("click", cellClick);
+  
+  let cellText = document.createElement("p");
+  cellText.innerHTML = `${roomNumber}`;
+  cell.append(cellText);
+  parent.append(cell);
+}
+
+function cellClick(event) {
+  if (editMode) {
+    let roomNumber = event.target.innerHTML;
+    cycleCellStatus(roomNumber);
+  }
+}
+
+// Set the status of each non-base status cell. Initially, this will mark the due-outs red or whatever.
+function setCellStatus(roomNumber, colorKey) {
+  let cell = document.querySelector(`#room-${roomNumber}`);
+  if (colorKey) {
+    cell.style.backgroundColor = roomColors[colorKey];  
+  }
+}
+
+function cycleCellStatus(roomNumber) {
+  let cell = document.querySelector(`#room-${roomNumber}`); 
+  console.log(cell.style.backgroundColor);
+  cell.style.backgroundColor = roomFSM(cell.style.backgroundColor);
+}
+
+// A Finite State Machine for changing a room cell's color.
+function roomFSM(state) {
+  switch(state) {
+    case roomColors.dueOut:
+      return roomColors.checkedOut;
+      break;
+    case roomColors.checkedOut:
+      return roomColors.earlyCheckout;
+      break;
+    case roomColors.earlyCheckout:
+      return roomColors.misc;
+      break;
+    case roomColors.misc:
+      return roomColors.dueOut;
+      break;
+    default:
+      return roomColors.misc;
+      break;
+  }
+}
+
 // Grab the items in the right textarea and append them to the left textarea.
 // Then sort and update the left textarea, make a log.
 oldButton.addEventListener("click", function() {
   // Append the checkouts list to the old list.
-  oldList.value = oldList.value + "\n" + newList.value;
-  
+  let appendedList = oldList.value + "\n" + newList.value;
   // Sort the whole thing again.
-  let list = sanitizeText(oldList.value);
+  let list = sanitizeText(appendedList, "\n");
   let sortedList = sortList(list);
-  console.log(sortedList);
-  // get the new checkouts. An optimization opportunity is to only sanitize and sort this one if I haven't
-  // already. Normal operation would have me sort the new checkouts anyways before I report them to the
-  // Housekeepers. For now, I'll do all text cleaning stuff anyway.
-  let changeList = sanitizeText(newList.value);
-  let sortedChangeList = sortList(changeList);
   let repeatsRemovedList = repeatCheck(sortedList);
-  
   let strList = arrayToList(repeatsRemovedList, "\n");
-  updateListBox(newList, strList);
+  updateListBox(oldList, strList);
+  
+  console.log(sortedList);
+  
+  // Get just the new checkouts. I could check if it has already been sorted.
+  // Todo: Have the changelist only show non-repeats relative to the main list.
+  let changeList = sanitizeText(newList.value, "\n");
+  let sortedChangeList = sortList(changeList);
   addLog(sortedList, changeList);
   
+  for (let cell of sortedList) {
+    setCellStatus(cell, "checkedOut");
+  }
+  
+  // Clear the new checkouts input box.
   newList.value = "";
 });
 
 newButton.addEventListener("click", function() {
-  let list = sanitizeText(newList.value);
+  let list = sanitizeText(newList.value, "\n");
   let sortedList = sortList(list);
   let repeatsRemovedList = repeatCheck(sortedList);
   updateListBox(newList, arrayToList(repeatsRemovedList, "\n"));
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  drawMatrix();
+});
+
+matrixEditButton.addEventListener("click", function() {
+  editMode = !editMode; // Toggle edit mode
+
+  let elements = document.querySelectorAll(".edit-mode-ui");
+  for (let element of elements) {
+    if (editMode) {
+      element.style.visibility = "visible"
+    }
+    else {
+      element.style.visibility = "hidden"
+    }
+  }
+  
+});
+
+matrixSetButton.addEventListener("click", function() {
+  let list = sanitizeText(setCheckoutInput.value, " ");
+  let sortedList = sortList(list);
+  for (let cell of sortedList) {
+    setCellStatus(cell, "dueOut");
+  }
+});
+
+testButton.addEventListener("click", function() { });
+
+testButton2.addEventListener("click", function() {
+  setCellStatus(300,"earlyCheckout");
 });
